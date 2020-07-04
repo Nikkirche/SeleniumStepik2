6af61dd5cd140cfc1232b798fcabb3968264d72f
Nikkirche <49571700+Nikkirche@users.noreply.github.com>
2020-07-04 23:41:03 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,6 +3,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_existing_of_add_button_into_cart(browser):
     browser.get(link)
-    buttons = browser.find_element_by_css_selector('[class="btn btn-lg btn-primary btn-add-to-basket"]')
-    print(buttons)
-    assert buttons is not None
+    button = browser.find_element_by_css_selector('[class="btn btn-lg btn-primary btn-add-to-basket"]')
+    assert button is not None
